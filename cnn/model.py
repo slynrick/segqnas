b4650601e3616968e2ee93d3609216454868c152
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-06 10:32:20 -0300

    .

--- cnn/model.py
+++ cnn/model.py
@@ -218,7 +218,7 @@ def get_segmentation_model(
     outputs = layers.Conv2D(
         filters=num_classes,
         kernel_size=1,
-        activation="softmax",
+        activation="sigmoid",
         padding="same",
         strides=1,
         data_format="channels_last",
