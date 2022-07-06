723944aacadc9da2aae5850f784a6ffe62ce7af9
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-06 10:18:26 -0300

    .

--- cnn/train.py
+++ cnn/train.py
@@ -319,7 +319,7 @@ def fitness_calculation(id_num, data_info, params, fn_dict, net_list):
     history = net.fit(train_data_generator, validation_data=val_data_generator)
 
     tf.compat.v1.logging.log(
-        level=tf.compat.v1.logging.get_verbosity(), msg=f"history {history()}"
+        level=tf.compat.v1.logging.get_verbosity(), msg=f"history {history}"
     )
 
     params["net"] = net
