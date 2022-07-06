208b17369464ba55f827d22a1d0c086380dad00d
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-06 10:57:24 -0300

    .

--- cnn/model.py
+++ cnn/model.py
@@ -180,7 +180,7 @@ def get_segmentation_model(
     input_shape, num_classes, fn_dict, net_list, is_train=True, mu=0.9, epsilon=2e-5
 ):
 
-    layer_dict = {}
+    '''layer_dict = {}
     for name, definition in fn_dict.items():
         if definition["function"] in ["ConvBlock"]:
             definition["params"]["mu"] = mu
@@ -227,6 +227,57 @@ def get_segmentation_model(
         name="final_conv",
     )(x)
 
-    model = Model(inputs=inputs, outputs=outputs)
+    model = Model(inputs=inputs, outputs=outputs)'''
+    inputs = Input(shape=input_shape)
+    # encoder: contracting path - downsample
+    # 1 - downsample
+    f1, p1 = downsample_block(inputs, 64)
+    # 2 - downsample
+    f2, p2 = downsample_block(p1, 128)
+    # 3 - downsample
+    f3, p3 = downsample_block(p2, 256)
+    # 4 - downsample
+    f4, p4 = downsample_block(p3, 512)
+    # 5 - bottleneck
+    bottleneck = double_conv_block(p4, 1024)
+    # decoder: expanding path - upsample
+    # 6 - upsample
+    u6 = upsample_block(bottleneck, f4, 512)
+    # 7 - upsample
+    u7 = upsample_block(u6, f3, 256)
+    # 8 - upsample
+    u8 = upsample_block(u7, f2, 128)
+    # 9 - upsample
+    u9 = upsample_block(u8, f1, 64)
+    # outputs
+    outputs = layers.Conv2D(num_classes, 1, padding="same", activation = "sigmoid")(u9)
+    # unet model with Keras Functional API
+    model = Model(inputs, outputs, name="U-Net")
 
     return model
+
+def double_conv_block(x, n_filters):
+   # Conv2D then ReLU activation
+   x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
+   # Conv2D then ReLU activation
+   x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
+   return x
+
+def downsample_block(x, n_filters):
+   f = double_conv_block(x, n_filters)
+   p = layers.MaxPool2D(2)(f)
+   p = layers.Dropout(0.3)(p)
+   return f, p
+
+def upsample_block(x, conv_features, n_filters):
+   # upsample
+   x = layers.Conv2DTranspose(n_filters, 3, 2, padding="same")(x)
+   # concatenate
+   x = layers.concatenate([x, conv_features])
+   # dropout
+   x = layers.Dropout(0.3)(x)
+   # Conv2D twice with ReLU activation
+   x = double_conv_block(x, n_filters)
+   return x
+
+def 
