32b008f3496944fce2b502f50b8437757f9df86e
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-07 00:21:46 -0300

    .

--- cnn/train.py
+++ cnn/train.py
@@ -309,7 +309,8 @@ def fitness_calculation(id_num, data_info, params, fn_dict, net_list):
     net.compile(
         optimizer=optimizer,
         loss='mse',#loss_function.DiceLoss(),
-        metrics=[UpdatedMeanIoU(num_classes=data_info.num_classes, name='mean_iou'), 
+        metrics=[Mean_IOU,
+                #UpdatedMeanIoU(num_classes=data_info.num_classes, name='mean_iou'), 
                 #tf.keras.metrics.MeanIoU(data_info.num_classes, name="mean_iou")
                 ],
     )
@@ -407,6 +408,30 @@ def fitness_calculation(id_num, data_info, params, fn_dict, net_list):
 
 import time
 
+from keras import backend as K
+
+
+def Mean_IOU(y_true, y_pred):
+    nb_classes = K.int_shape(y_pred)[-1]
+    y_pred = K.reshape(y_pred, (-1, nb_classes))
+    y_true = tf.to_int32(K.reshape(y_true, (-1, 1))[:,0])
+    y_true = K.one_hot(y_true, nb_classes)
+    true_pixels = K.argmax(y_true, axis=-1) # exclude background
+    pred_pixels = K.argmax(y_pred, axis=-1)
+    iou = []
+    flag = tf.convert_to_tensor(-1, dtype='float64')
+    for i in range(nb_classes-1):
+        true_labels = K.equal(true_pixels, i)
+        pred_labels = K.equal(pred_pixels, i)
+        inter = tf.to_int32(true_labels & pred_labels)
+        union = tf.to_int32(true_labels | pred_labels)
+        cond = (K.sum(union) > 0) & (K.sum(tf.to_int32(true_labels)) > 0)
+        res = tf.cond(cond, lambda: K.sum(inter)/K.sum(union), lambda: flag)
+        iou.append(res)
+    iou = tf.stack(iou)
+    legal_labels = tf.greater(iou, flag)
+    iou = tf.gather(iou, indices=tf.where(legal_labels))
+    return K.mean(iou)
 
 class UpdatedMeanIoU(tf.keras.metrics.MeanIoU):
   def __init__(self,
@@ -423,6 +448,7 @@ class UpdatedMeanIoU(tf.keras.metrics.MeanIoU):
         level=tf.compat.v1.logging.get_verbosity(), msg=f"y_true {y_true}, y_pred {y_pred}"
     )
 
+    y_true = tf.math.argmax(y_true, axis=-1)
     y_pred = tf.math.argmax(y_pred, axis=-1)
 
     tf.compat.v1.logging.log(
