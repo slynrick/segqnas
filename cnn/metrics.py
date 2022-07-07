7aa795fbfff728a3e94301c8dd02b07e641a5edb
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-07 10:14:16 -0300

    .

--- cnn/metrics.py
+++ cnn/metrics.py
@@ -5,72 +5,7 @@ from cnn.helpers import (average, gather_channels, get_reduce_axes,
 
 SMOOTH = 1e-5
 
-class KerasObject:
-    _backend = None
-    _models = None
-    _layers = None
-    _utils = None
-
-    def __init__(self, name=None):
-        if (self.backend is None or
-                self.utils is None or
-                self.models is None or
-                self.layers is None):
-            raise RuntimeError('You cannot use `KerasObjects` with None submodules.')
-
-        self._name = name
-
-    @property
-    def __name__(self):
-        if self._name is None:
-            return self.__class__.__name__
-        return self._name
-
-    @property
-    def name(self):
-        return self.__name__
-
-    @name.setter
-    def name(self, name):
-        self._name = name
-
-    @classmethod
-    def set_submodules(cls, backend, layers, models, utils):
-        cls._backend = backend
-        cls._layers = layers
-        cls._models = models
-        cls._utils = utils
-
-    @property
-    def submodules(self):
-        return {
-            'backend': self.backend,
-            'layers': self.layers,
-            'models': self.models,
-            'utils': self.utils,
-        }
-
-    @property
-    def backend(self):
-        return self._backend
-
-    @property
-    def layers(self):
-        return self._layers
-
-    @property
-    def models(self):
-        return self._models
-
-    @property
-    def utils(self):
-        return self._utils
-
-
-class Metric(KerasObject):
-    pass
-
-class IOUScore(Metric):
+class IOUScore:
     r"""The `Jaccard index`_, also known as Intersection over Union and the Jaccard similarity coefficient
     (originally coined coefficient de communauté by Paul Jaccard), is a statistic used for comparing the
     similarity and diversity of sample sets. The Jaccard coefficient measures similarity between finite sample sets,
