928908863111dc902f1b5ec6d23c6a720a94d7ad
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-08 00:35:06 -0300

    .

--- cnn/train.py
+++ cnn/train.py
@@ -340,7 +340,7 @@ def fitness_calculation(id_num, params, fn_dict, net_list):
     )
 
     net.compile(
-        optimizer="Adam",  # optimizer,
+        optimizer=optimizer,
         loss=sm.losses.categorical_focal_dice_loss,
         metrics=[
             sm.metrics.IOUScore(threshold=0.5)
