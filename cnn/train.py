4d3d7c8d352ad9aa4d5499e37c9180ca0d8f4dba
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-07-07 22:19:56 -0300

    .

--- cnn/train.py
+++ cnn/train.py
@@ -442,17 +442,3 @@ def fitness_calculation(id_num, params, fn_dict, net_list):
 #
 #    return mean_iou
 #
-
-
-class UpdatedMeanIoU(tf.keras.metrics.MeanIoU):
-    def __init__(
-        self, y_true=None, y_pred=None, num_classes=None, name=None, dtype=None
-    ):
-        super(UpdatedMeanIoU, self).__init__(
-            num_classes=num_classes, name=name, dtype=dtype
-        )
-
-    def update_state(self, y_true, y_pred, sample_weight=None):
-        y_pred = tf.math.argmax(y_pred, axis=-1)
-        y_pred = tf.one_hot(indices=y_pred, depth=self.num_classes)
-        return super().update_state(y_true, y_pred, sample_weight)
