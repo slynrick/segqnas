8e513ca6d7378a3e58226242a9fd12526ceb387d
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-08-09 13:34:04 -0300

    .

--- cnn/train.py
+++ cnn/train.py
@@ -53,9 +53,15 @@ def fitness_calculation(id_num, params, fn_dict, net_list):
 
     tf.config.experimental.set_visible_devices(gpus[gpu_id], "GPU")
     
+    # try:
+    #     for gpu in gpus:
+    #         tf.config.experimental.set_memory_growth(gpu, True)
+    # except RuntimeError as e:
+    #     print(e)
+
     try:
         for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
+            tf.config.experimental.set_virtual_device_configuration(gpus, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=20480)])
     except RuntimeError as e:
         print(e)
 
