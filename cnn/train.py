79a0608df7774eb73c2b0140d22f2a450d39b274
Guilherme Baldo Carlos <guilhermebaldocarlos@gmail.com>
2022-08-09 11:30:05 -0300

    .

--- cnn/train.py
+++ cnn/train.py
@@ -52,10 +52,17 @@ def fitness_calculation(id_num, params, fn_dict, net_list):
     gpu_id = int(id_num.split("_")[-1])%len(gpus)
 
     tf.config.experimental.set_visible_devices(gpus[gpu_id], "GPU")
+    
     try:
-        tf.config.experimental.set_virtual_device_configuration(gpus[gpu_id], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3072)])
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
     except RuntimeError as e:
         print(e)
+
+    # try:
+    #     tf.config.experimental.set_virtual_device_configuration(gpus[gpu_id], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3072)])
+    # except RuntimeError as e:
+    #     print(e)
 
     # filtered_dict = {key: item for key, item in fn_dict.items() if key in net_list}
     tf.compat.v1.logging.log(
