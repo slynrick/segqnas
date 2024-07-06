2902b4003c7d14532eb9dd22f25c75f5d78d07fb
Fabio Cardoso <slynrick@gmail.com>
2024-07-06 16:20:15 -0300

    correct self attention

--- cnn/custom_layers.py
+++ cnn/custom_layers.py
@@ -1,24 +1,20 @@
-from keras.layers import Layer, Conv2D, Activation
+from keras.layers import Layer, Conv2D, Activation, Multiply
 import tensorflow as tf
 
 
 class SelfAttentionLayer(Layer):
     def __init__(self, **kwargs):
         super(SelfAttentionLayer, self).__init__(**kwargs)
-        self.channels = -1
+        self.filters = -1
         self.query = None
         self.key = None
         self.value = None
-        self.gamma = None
 
     def build(self, input_shape):
-        self.channels = input_shape[-1]
-        self.query = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
-        self.key = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
-        self.value = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
-        self.gamma = self.add_weight(shape=(self.channels,),
-                                  initializer='glorot_normal',
-                                  regularizer=tf.keras.regularizers.l2(1e-4))
+        self.filters = input_shape[-1]
+        self.query = Conv2D(filters=self.filters, kernel_size=1, strides=1, padding='same')
+        self.key = Conv2D(filters=self.filters, kernel_size=1, strides=1, padding='same')
+        self.value = Conv2D(filters=self.filters, kernel_size=1, strides=1, padding='same')
         super(SelfAttentionLayer, self).build(input_shape)
 
     def call(self, inputs):
@@ -26,10 +22,10 @@ class SelfAttentionLayer(Layer):
         key = self.key(inputs)
         value = self.value(inputs)
 
-        attention = tf.matmul(query, key, transpose_b=True) 
+        attention = tf.matmul(key, query, transpose_b=True) 
         attention = Activation('softmax')(attention)
 
-        out = tf.matmul(attention, value)
+        output = tf.matmul(attention, value)
 
-        out = self.gamma * out + inputs
-        return out
+        output = Multiply()([output, inputs])
+        return output
