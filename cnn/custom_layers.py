7ed291ad7856905a3acf0a35d9a7812610f5d538
Fabio Cardoso <slynrick@gmail.com>
2024-07-06 16:01:46 -0300

    correct self attention

--- cnn/custom_layers.py
+++ cnn/custom_layers.py
@@ -1,4 +1,4 @@
-from keras.layers import Layer, Conv2D, Activation, Multiply
+from keras.layers import Layer, Conv2D, Activation
 import tensorflow as tf
 
 
@@ -6,26 +6,30 @@ class SelfAttentionLayer(Layer):
     def __init__(self, **kwargs):
         super(SelfAttentionLayer, self).__init__(**kwargs)
         self.channels = -1
-        self.filters_f = None
-        self.filters_g = None
-        self.filters_h = None
+        self.query = None
+        self.key = None
+        self.value = None
+        self.gamma = None
 
     def build(self, input_shape):
         self.channels = input_shape[-1]
-        self.filters_f = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
-        self.filters_g = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
-        self.filters_h = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
+        self.query = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
+        self.key = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
+        self.value = Conv2D(filters=self.channels, kernel_size=1, strides=1, padding='same')
+        self.gamma = self.add_weight(shape=(self.channels,),
+                                  initializer='glorot_normal',
+                                  regularizer=tf.keras.regularizers.l2(1e-4))
         super(SelfAttentionLayer, self).build(input_shape)
 
     def call(self, inputs):
-        f = self.filters_f(inputs)
-        g = self.filters_g(inputs)
-        h = self.filters_h(inputs)
+        query = self.query(inputs)
+        key = self.key(inputs)
+        value = self.value(inputs)
 
-        s = tf.matmul(g, f, transpose_b=True) 
-        beta = Activation('softmax')(s)
+        attention = tf.matmul(query, key, transpose_b=True) 
+        attention = Activation('softmax')(attention)
 
-        o = tf.matmul(beta, h)
+        out = tf.matmul(attention, value)
 
-        x = Multiply()([o, inputs])
-        return x
+        out = self.gamma * out + inputs
+        return out
